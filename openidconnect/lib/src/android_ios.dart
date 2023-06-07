497ac47bc2bd155ba406cccb986c7885cc61ac1b
mobikats <mike@mobikats.com>
2023-06-07 12:49:22 +0200

    Update android_ios.dart onPageFinished updates testing

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -10,53 +10,36 @@ class OpenIdConnectAndroidiOS {
     required int popupHeight,
   }) async {
     //Create the url
-  bool loggedin = false;
+    bool loggedin = false;
     final result = await showDialog<String?>(
       context: context,
       barrierDismissible: false,
       builder: (dialogContext) {
-        return AlertDialog(
-          actions: [
-            IconButton(
-              onPressed: () => Navigator.pop(dialogContext, null),
-              icon: Icon(Icons.close),
-            ),
-          ],
-          content: Container(
-            width:
-                min(popupWidth.toDouble(), MediaQuery.of(context).size.width),
-            height:
-                min(popupHeight.toDouble(), MediaQuery.of(context).size.height),
-            child: flutterWebView.WebView(
-              javascriptMode: flutterWebView.JavascriptMode.unrestricted,
-              initialUrl: authorizationUrl,
+        return flutterWebView.WebView(
+          javascriptMode: flutterWebView.JavascriptMode.unrestricted,
+          initialUrl: authorizationUrl,
+          onPageFinished: (url) {
             if (url.startsWith(redirectUrl)) {
               Navigator.pop(dialogContext, url);
               return;
             }
-                    setState(() {
-                    print("loggedin " + loggedIn.toString());
+            setState(() {
+            print("loggedin " + loggedIn.toString());
 
-                    if(loggedIn == false) {
-                      loggedIn = true;
-                        _controller.future
-                            .then((value) =>
-                            value.evaluateJavascript('''
-                             var email = document.getElementById("CustomerEmail");
-                             var password = document.getElementById("CustomerPassword");
-                             email.value = "user@gmail.com";
-                             password.value = "test123";
-                          //   document.getElementById('customer_login').submit();
-                           '''));
-                        
-                      }
-
-                      
-                    });
-                  },
-            ),
-          ),
-          title: Text(title),
+            if(loggedIn == false) {
+              loggedIn = true;
+                _controller.future
+                    .then((value) =>
+                    value.evaluateJavascript('''
+                     var email = document.getElementById("CustomerEmail");
+                     var password = document.getElementById("CustomerPassword");
+                     email.value = "user@gmail.com";
+                     password.value = "test123";
+                  //   document.getElementById('customer_login').submit();
+                   '''));
+              }
+            });
+          },
         );
       },
     );
